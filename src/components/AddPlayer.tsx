4b890402ae83372c1aae30d111b90697cce97aba
timand1 <80768929+timand1@users.noreply.github.com>
2022-09-27 14:09:55 +0200

    Fix wrong index sent in handleTeam

--- src/components/AddPlayer.tsx
+++ src/components/AddPlayer.tsx
@@ -7,16 +7,21 @@ interface AddPlayerProps {
 
 
 export default function DisplayTeams(props: AddPlayerProps) {    
-    
+    let playerIndex = props.playerNum - 1;
+
+    if(playerIndex > 4) {
+        playerIndex = playerIndex - 5;
+    };
+
     return (         
         <div className="teams">
         <div>
             <p>Player {props.playerNum} {props.required ? '*' : ''} </p>
-            <input type="text" name="player" placeholder={`Player ${props.playerNum} name`} id={`player-${props.playerNum}`} defaultValue={props.player[`player-${props.playerNum}`]} required={props.required} onKeyUp={(e) => {props.handleTeam(e, 0)}}/>
+            <input type="text" name="player" placeholder={`Player ${props.playerNum} name`} id={`player-${props.playerNum}`} defaultValue={props.player[`player-${props.playerNum}`]} required={props.required} onKeyUp={(e) => {props.handleTeam(e, playerIndex)}}/>
         </div>
         <div>
             <p>Player {props.playerNum} info</p>
-            <textarea rows={2} name="player-info" placeholder={`Player ${props.playerNum} info`} id={`player-${props.playerNum}-info`} defaultValue={props.player[`player-${props.playerNum}-info`]} onKeyUp={(e) => {props.handleTeam(e, 0)}}/>
+            <textarea rows={2} name="player-info" placeholder={`Player ${props.playerNum} info`} id={`player-${props.playerNum}-info`} defaultValue={props.player[`player-${props.playerNum}-info`]} onKeyUp={(e) => {props.handleTeam(e, playerIndex)}}/>
         </div>
     </div>
     )
