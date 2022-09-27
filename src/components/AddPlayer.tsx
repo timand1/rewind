bbbc1a7c7ab21f836eabb320b1264d17692f86ac
timand1 <80768929+timand1@users.noreply.github.com>
2022-09-27 13:15:25 +0200

    Fix formatting errors AddPlayer

--- src/components/AddPlayer.tsx
+++ src/components/AddPlayer.tsx
@@ -12,11 +12,11 @@ export default function DisplayTeams(props: AddPlayerProps) {
         <div className="teams">
         <div>
             <p>Player {props.playerNum} {props.required ? '*' : ''} </p>
-            <input type="text" name="player" placeholder={`Player${props.playerNum} name`} id={`player${props.playerNum}`} defaultValue={props.player[`player-${props.playerNum}`]} required={props.required} onKeyUp={(e) => {props.handleTeam(e, 0)}}/>
+            <input type="text" name="player" placeholder={`Player ${props.playerNum} name`} id={`player-${props.playerNum}`} defaultValue={props.player[`player-${props.playerNum}`]} required={props.required} onKeyUp={(e) => {props.handleTeam(e, 0)}}/>
         </div>
         <div>
             <p>Player {props.playerNum} info</p>
-            <textarea rows={2} name="player-info" placeholder={`Player ${props.playerNum} info`} id="player-1-info" defaultValue={props.player[`player-${props.playerNum}-info`]} onKeyUp={(e) => {props.handleTeam(e, 0)}}/>
+            <textarea rows={2} name="player-info" placeholder={`Player ${props.playerNum} info`} id={`player-${props.playerNum}-info`} defaultValue={props.player[`player-${props.playerNum}-info`]} onKeyUp={(e) => {props.handleTeam(e, 0)}}/>
         </div>
     </div>
     )
